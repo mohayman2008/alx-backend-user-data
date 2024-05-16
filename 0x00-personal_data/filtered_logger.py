d8583230145aab294feb65f99975b08ddf856a4f
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-16 01:49:21 +0000

    0x00. Personal data - Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''The module implements logger that filters out PII data out'''
+import logging
 import re
 from typing import List
 
@@ -16,3 +17,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
         message = re.sub(pattern, replacement, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    '''Redacting Formatter class'''
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''The method filters out the values of "self.fields" in the incoming
+        log records using the helper function "filter_datum"'''
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg,
+                                  self.SEPARATOR)
+        return super().format(record)
