34e2c6692910a9fb0dd7771a50aa7ad94b14e210
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-16 02:45:51 +0000

    0x00. Personal data - Task 2 - Using different "PII_FIELDS"

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import logging
 import re
 from typing import List
 
-PII_FIELDS = ("email", "phone", "ssn", "password", "ip")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
