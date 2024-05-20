0cac94bc4d5eb0ee63a7fc3155a301efe0f66be4
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-20 22:15:51 +0000

    0x01. Basic authentication - Mandatory tasks 10 & 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,10 @@
 '''Basic Athentication Module'''
 from base64 import b64decode
 import binascii
+from typing import TypeVar
 
-from .auth import Auth
+from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -29,7 +31,8 @@ class BasicAuth(Auth):
         if b64ah is None or type(b64ah) != str:
             return None
         try:
-            return b64decode(base64_authorization_header).decode("utf-8")
+            decoded = b64decode(base64_authorization_header.encode("utf-8"))
+            return decoded.decode("utf-8")
         except binascii.Error:
             return None
 
@@ -42,3 +45,24 @@ class BasicAuth(Auth):
         if val is None or type(val) != str or val.find(':') < 0:
             return None, None
         return tuple(val.split(":", 1))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        '''Returns the User instance based on email and password'''
+        email_pwd = [user_email, user_pwd]
+        if None in email_pwd or any(map(lambda x: type(x) != str, email_pwd)):
+            return None
+        user_search_results = User.search({"email": user_email})
+        if not len(user_search_results):
+            return None
+        user = user_search_results[0]
+
+        return user if user.is_valid_password(user_pwd) else None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''Retrieves the "User" instance for a request'''
+        auth_header = self.authorization_header(request)
+        b64 = self.extract_base64_authorization_header(auth_header)
+        auth_decoded = self.decode_base64_authorization_header(b64)
+        email, pwd = self.extract_user_credentials(auth_decoded)
+        return self.user_object_from_credentials(email, pwd)
